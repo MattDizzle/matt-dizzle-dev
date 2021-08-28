eadc2ba17049d1e0a64ab546f5ec5ec0d31c7b9e
Matthew Rougely <matthewrougelysr@gmail.com>
2021-08-27 23:15:33 -0700

    Adding logos to skills, modifying Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,16 @@
 import React from 'react'
 
 export default function Card(props) {
-
+    
     return (
         <section className={`card animate__animated animate__backInUp ${props.rev}`}>
             <img src= {props.source} className='card-pic' alt='some text' />
             <div className='card-text-box'>
             <h2>{props.title}</h2>
-            <p>  Sunt eu non et ut est. Nisi quis ea velit dolor aliqua pariatur sit esse magna cillum occaecat. Cupidatat id proident aliqua non. Duis non exercitation veniam minim amet cillum excepteur ipsum nisi do adipisicing. Cupidatat id sint excepteur sunt laboris. Irure exercitation anim Lorem sunt quis tempor consectetur mollit. Enim cillum cillum occaecat culpa excepteur qui.   </p>
+            <div className='icon-box'>
+            {props.iconz}
+            </div>
+            <p>{props.paragraph}</p>
             </div>
         </section>
     )
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,14 +2,21 @@ import React from 'react'
 import Card from './Card'
 
 const Home = () => {
+
+    const skillsIcon = <>
+        <img src="https://img.icons8.com/office/100/000000/react.png"/>
+        <img src="https://img.icons8.com/color/100/000000/nodejs.png"/>
+        <img src="https://img.icons8.com/ios-filled/50/000000/adobe-logo.png"/>
+        <img src="https://img.icons8.com/ios-glyphs/60/000000/adobe-photoshop.png"/>
+    </>
     return (
         <section>
             <Card source='https://source.unsplash.com/collection/10649550' title='about me' paragraph='lorem ipsum and thangs for now just rambling' id='about-me' />
-            <Card source='https://source.unsplash.com/collection/1773435' rev = 'reverse' title='skills' paragraph='lorem ipsum and thangs for now just rambling' id='skills' />
+            <Card source='https://source.unsplash.com/collection/1773435' rev='reverse' title='skills' paragraph='' iconz={skillsIcon} id='skills' />
             <Card source='https://source.unsplash.com/collection/8354564' title='passions' paragraph='lorem ipsum and thangs for now just rambling' id='passions' />
-            <Card source='https://source.unsplash.com/collection/6221583' rev = 'reverse' title='how I learned stuff' paragraph='lorem ipsum and thangs for now just rambling' />
+            <Card source='https://source.unsplash.com/collection/6221583' rev='reverse' title='how I learned stuff' paragraph='lorem ipsum and thangs for now just rambling' />
         </section>
-        
+
     )
 }
 
