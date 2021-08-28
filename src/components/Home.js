26885c3c8d639ec66afc9c79de55f9ef20f1d022
Matthew Rougely <matthewrougelysr@gmail.com>
2021-08-28 07:46:36 -0700

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -21,8 +21,6 @@ const Home = () => {
                 Development using PostgreSQL, Express, React and Node.js through hands on learning, building multiple apps. "
             id='about-me' />
             <Card source='https://source.unsplash.com/collection/1773435' rev='reverse' title='skills' paragraph='' iconz={skillsIcon} id='skills' />
-            <Card source='https://source.unsplash.com/collection/8354564' title='passions' paragraph='lorem ipsum and thangs for now just rambling' id='passions' />
-            <Card source='https://source.unsplash.com/collection/6221583' rev='reverse' title='how I learned stuff' paragraph='lorem ipsum and thangs for now just rambling' />
         </section>
 
     )
