3366b78face60180f2db292d8a3359a64da623bb
Matthew Rougely <matthewrougelysr@gmail.com>
2021-08-28 08:12:09 -0700

    adding graphics

--- src/components/PhotoGallery.js
+++ src/components/PhotoGallery.js
@@ -1,10 +1,39 @@
 import { ProGallery } from 'pro-gallery';
 import 'pro-gallery/dist/statics/main.css';
+import  one  from "../images/2.jpg"
+import  two  from "../images/3 (1).jpg"
+import  three  from "../images/3 (2).jpg"
+import  four  from "../images/3 (3).jpg"
+import  five  from "../images/5.jpg"
+import  six  from "../images/6.jpg"
+import  seven  from "../images/7.jpg"
+import  eight  from "../images/8.jpg"
+import  nine  from "../images/9.jpg"
+import  ten  from "../images/10.jpg"
+import  eleven  from "../images/11.jpg"
+import  twelve  from "../images/12.jpg"
+
 export default function PhotoGallery() {
     const items = [
         { 
             itemId: 'sample-id',
-            mediaUrl: 'https://i.picsum.photos/id/674/200/300.jpg?hmac=kS3VQkm7AuZdYJGUABZGmnNj_3KtZ6Twgb5Qb9ITssY',
+            mediaUrl: one,
+            metaData: {
+                type: 'image',
+                height: 200,
+                width: 100,
+                title: 'sample-title',
+                description: 'sample-description',
+                focalPoint: [0, 0],
+                link: {
+                    url: '../images/12.jpg',
+                    target: '_blank'
+                },
+            }
+        },
+        { 
+            itemId: 'differentItem',
+            mediaUrl: two,
             metaData: {
                 type: 'image',
                 height: 200,
@@ -20,7 +49,7 @@ export default function PhotoGallery() {
         },
         { 
             itemId: 'differentItem',
-            mediaUrl: 'https://i.picsum.photos/id/1003/1181/1772.jpg?hmac=oN9fHMXiqe9Zq2RM6XT-RVZkojgPnECWwyEF1RvvTZk',
+            mediaUrl: three,
             metaData: {
                 type: 'image',
                 height: 200,
@@ -34,19 +63,151 @@ export default function PhotoGallery() {
                 },
             }
         },
-        { // HTML item:
-            itemId: 'htmlItem',
-            html: "<div style='width: 300px; height: 200px; background:pink;'>I am a text block</div>",
-            metadata: {
-                type: "text",
+        { 
+            itemId: 'differentItem',
+            mediaUrl: four,
+            metaData: {
+                type: 'image',
                 height: 200,
-                width: 300,
+                width: 100,
                 title: 'sample-title',
                 description: 'sample-description',
-                backgroundColor: 'pink'
-            },
-
+                focalPoint: [0, 0],
+                link: {
+                    url: 'http://example.com',
+                    target: '_blank'
+                },
+            }
         },
+        { 
+            itemId: 'differentItem',
+            mediaUrl: five,
+            metaData: {
+                type: 'image',
+                height: 200,
+                width: 100,
+                title: 'sample-title',
+                description: 'sample-description',
+                focalPoint: [0, 0],
+                link: {
+                    url: 'http://example.com',
+                    target: '_blank'
+                },
+            }
+        },
+        { 
+            itemId: 'differentItem',
+            mediaUrl: six,
+            metaData: {
+                type: 'image',
+                height: 200,
+                width: 100,
+                title: 'sample-title',
+                description: 'sample-description',
+                focalPoint: [0, 0],
+                link: {
+                    url: 'http://example.com',
+                    target: '_blank'
+                },
+            }
+        },
+        { 
+            itemId: 'differentItem',
+            mediaUrl: seven,
+            metaData: {
+                type: 'image',
+                height: 200,
+                width: 100,
+                title: 'sample-title',
+                description: 'sample-description',
+                focalPoint: [0, 0],
+                link: {
+                    url: 'http://example.com',
+                    target: '_blank'
+                },
+            }
+        },
+        { 
+            itemId: 'differentItem',
+            mediaUrl: eight,
+            metaData: {
+                type: 'image',
+                height: 200,
+                width: 100,
+                title: 'sample-title',
+                description: 'sample-description',
+                focalPoint: [0, 0],
+                link: {
+                    url: 'http://example.com',
+                    target: '_blank'
+                },
+            }
+        },
+        { 
+            itemId: 'differentItem',
+            mediaUrl: nine,
+            metaData: {
+                type: 'image',
+                height: 200,
+                width: 100,
+                title: 'sample-title',
+                description: 'sample-description',
+                focalPoint: [0, 0],
+                link: {
+                    url: 'http://example.com',
+                    target: '_blank'
+                },
+            }
+        },
+        { 
+            itemId: 'differentItem',
+            mediaUrl: ten,
+            metaData: {
+                type: 'image',
+                height: 200,
+                width: 100,
+                title: 'sample-title',
+                description: 'sample-description',
+                focalPoint: [0, 0],
+                link: {
+                    url: 'http://example.com',
+                    target: '_blank'
+                },
+            }
+        },
+        { 
+            itemId: 'differentItem',
+            mediaUrl: eleven,
+            metaData: {
+                type: 'image',
+                height: 200,
+                width: 100,
+                title: 'sample-title',
+                description: 'sample-description',
+                focalPoint: [0, 0],
+                link: {
+                    url: 'http://example.com',
+                    target: '_blank'
+                },
+            }
+        },
+        { 
+            itemId: 'differentItem',
+            mediaUrl: twelve,
+            metaData: {
+                type: 'image',
+                height: 200,
+                width: 100,
+                title: 'sample-title',
+                description: 'sample-description',
+                focalPoint: [0, 0],
+                link: {
+                    url: 'http://example.com',
+                    target: '_blank'
+                },
+            }
+        },
+        
     ]
 
     // The options of the gallery (from the playground current state)
