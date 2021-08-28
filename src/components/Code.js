25d15c54fa2d7de0f84a1e17b19358c1ad8c16cc
Matthew Rougely <matthewrougelysr@gmail.com>
2021-08-27 18:00:36 -0700

    adding code content

--- src/components/Code.js
+++ src/components/Code.js
@@ -4,10 +4,10 @@ import Card from './Card'
 export default function Code() {
     return (
         <div>
-            <Card title='Saneful' paragraph='Created using Node.js, React, Express and Postgres'/>
-            <Card rev = 'reverse' title='Saneful' paragraph='Created using Node.js, React, Express and Postgres'/>
-            <Card title='Saneful' paragraph='Created using Node.js, React, Express and Postgres'/>
-            <Card rev = 'reverse' paragraph='Created using Node.js, React, Express and Postgres'/>
+            <Card source='https://raw.githubusercontent.com/MattDizzle/favicon-host/master/saneful/gameview.JPG' title='Saneful' paragraph='Created using Node.js, React, Express and Postgres'/>
+            <Card source='https://github.com/M-Sayer/spaced-repetition/blob/master/screenshots/Dash.JPG' rev = 'reverse' title='Spaced Repetition' paragraph='Created using Node.js, React, Express and Postgres'/>
+            <Card source='https://github.com/MattDizzle/my-poll-data-client/blob/main/screenshots/Elections.JPG' title='My Poll Data' paragraph='Created using Node.js, React, Express and Postgres'/>
+            <Card source='https://github.com/MattDizzle/weatherwatch-client/blob/main/Screenshots/Home.JPG' rev = 'Weather Watch' paragraph='Created using Node.js and React'/>
         </div>
     )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,9 +5,9 @@ const Home = () => {
     return (
         <section>
             <Card source='https://source.unsplash.com/collection/1459961' title='about me' paragraph='lorem ipsum and thangs for now just rambling' id='about-me' />
-            <Card rev = 'reverse' title='skills' paragraph='lorem ipsum and thangs for now just rambling' id='skills' />
-            <Card title='passions' paragraph='lorem ipsum and thangs for now just rambling' id='passions' />
-            <Card rev = 'reverse' title='how I learned stuff' paragraph='lorem ipsum and thangs for now just rambling' />
+            <Card source='https://source.unsplash.com/collection/1459961' rev = 'reverse' title='skills' paragraph='lorem ipsum and thangs for now just rambling' id='skills' />
+            <Card source='https://source.unsplash.com/collection/1459961' title='passions' paragraph='lorem ipsum and thangs for now just rambling' id='passions' />
+            <Card source='https://source.unsplash.com/collection/1459961' rev = 'reverse' title='how I learned stuff' paragraph='lorem ipsum and thangs for now just rambling' />
         </section>
         
     )
