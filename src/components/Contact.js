de14453c7be4a0a3c81e738fd8dd1b6db0163553
Matthew Rougely <matthewrougelysr@gmail.com>
2021-08-29 03:26:45 -0700

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,7 @@ export default function Contact() {
         <section className='contact-form-page' id='contact'>
             <div className='image-phone'>
             <img src= 'https://github.com/MattDizzle/favicon-host/blob/master/matt-dizzle-dev.JPG?raw=true' className='contact-pic' alt='matt dizzle dev' />
-            <a href="tel:+9095479183" id='phone-link'>Call Me: (909)547-9183</a>
+            <a href="tel:+19095479183" id='phone-link'>Call Me: (909)547-9183</a>
             </div>
 
            <ContactForm />
