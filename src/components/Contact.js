64ffd42c16df5379b26a69f867ebbffd332fb0ac
Matthew Rougely <matthewrougelysr@gmail.com>
2021-08-29 02:07:51 -0700

    styling contact page and added image

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,8 +3,8 @@ import ContactForm from './ContactForm'
 
 export default function Contact() {
     return (
-        <section className='contact-form' id='contact'>
-            <img src= 'https://source.unsplash.com/collection/10649550' className='contact-pic' alt='matt dizzle dev' />
+        <section className='contact-form-page' id='contact'>
+            <img src= 'https://github.com/MattDizzle/favicon-host/blob/master/matt-dizzle-dev.JPG?raw=true' className='contact-pic' alt='matt dizzle dev' />
            <ContactForm />
         </section>
     )
