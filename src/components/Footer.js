17a7e1bf54510e5ea93e4f15d9e0529ad0912533
Matthew Rougely <matthewrougelysr@gmail.com>
2021-08-29 00:37:53 -0700

    working out the footer spacing in desktop

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,31 +1,32 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowUp  } from '@fortawesome/free-solid-svg-icons'
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 library.add(fab)
 
 const Footer = () => {
-  const scrollToTop = () =>{
+  const scrollToTop = () => {
     window.scrollTo({
-      top: 0, 
+      top: 0,
       behavior: 'smooth'
     });
   };
-    return (
-        <footer className="site-footer">
-             <div className="container">
+  return (
+    <footer className="site-footer">
+      <div className="container">
         <div className="row">
-          <div className="col-sm-12 col-md-6">
+          
+          <div className="footer-about-me">
             <h6>About</h6>
-            <p className="text-justify">My name is Matthew and I live in Rialto, California. I'm an aspiring full stack web developer.
-             I love music, basketball, learning and computers. I recently did an Internship at Eide Bailly where I learned
+            <p className="footer-about-me-text">My name is Matthew and I live in Rialto, California. I'm an aspiring full stack web developer.
+              I love music, basketball, learning and computers. I recently did an Internship at Eide Bailly where I learned
               a ton of stuff about Integrating Multiple systems using a combination of Boomi and SuiteScript 2.0.
-               Prior to this Internship I graduated from the Engineering Immersion Program where I learned Full-Stack Web
-                Development using PostgreSQL, Express, React and Node.js through hands on learning, building multiple apps. </p>
+              Prior to this Internship I graduated from the Engineering Immersion Program where I learned Full-Stack Web
+              Development using PostgreSQL, Express, React and Node.js through hands on learning, building multiple apps. </p>
           </div>
 
-          <div className="col-xs-6 col-md-3">
+          <div className="categories">
             <h6>Categories</h6>
             <ul className="footer-links">
               <li><a href="code">JavaScript</a></li>
@@ -37,7 +38,7 @@ const Footer = () => {
             </ul>
           </div>
 
-          <div className="col-xs-6 col-md-3">
+          <div className="quick-links">
             <h6>Quick Links</h6>
             <ul className="footer-links">
               <li><a href="/">About Me</a></li>
@@ -47,27 +48,28 @@ const Footer = () => {
               <li><a href="/">Sitemap</a></li>
             </ul>
           </div>
+
         </div>
-        <hr/>
+        <hr />
       </div>
       <div className="container">
         <div className="row">
           <div className="">
-            <p className="copyright-text">Copyright &copy; 2021 All Rights Reserved by  
-          Matt Dizzle Dev.
+            <p className="copyright-text">Copyright &copy; 2021 All Rights Reserved by
+              Matt Dizzle Dev.
             </p>
           </div>
 
           <div className="social-icons">
-              <a href='https://linkedin.com/in/matthew-rougely' target='blank' ><i className ="fab fa-linkedin-in icon"/></a>
-              <a href='https://github.com/MattDizzle' target='blank' ><i className="fab fa-github icon" /></a>
-              <FontAwesomeIcon onClick={scrollToTop} className='icon'  icon={faArrowUp} />
+            <a href='https://linkedin.com/in/matthew-rougely' target='blank' ><i className="fab fa-linkedin-in icon" /></a>
+            <a href='https://github.com/MattDizzle' target='blank' ><i className="fab fa-github icon" /></a>
+            <FontAwesomeIcon onClick={scrollToTop} className='icon' icon={faArrowUp} />
           </div>
         </div>
       </div>
-        </footer>
-    )
-    
+    </footer>
+  )
+
 }
 
 export default Footer
