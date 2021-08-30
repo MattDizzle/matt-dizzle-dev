4ab69bb65ebad6c2ab9678fb4fa3f8f62967137c
Matthew Rougely <matthewrougelysr@gmail.com>
2021-08-29 18:48:40 -0700

    updating skills with new icons

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,28 +4,31 @@ import Card from './Card'
 const Home = () => {
 
     const skillsIcon = <>
-        <img alt='react logo' src="https://img.icons8.com/office/100/000000/react.png"/>
-        <img alt='nodejs logo' src="https://img.icons8.com/color/100/000000/nodejs.png"/>
-        <img alt='adobe logo' src="https://img.icons8.com/ios-filled/50/000000/adobe-logo.png"/>
-        <img alt='photoshop logo' src="https://img.icons8.com/ios-glyphs/60/000000/adobe-photoshop.png"/>
-        <img alt='photoshop logo' src="https://img.icons8.com/ios-glyphs/60/000000/adobe-photoshop.png"/>
-        <img alt='photoshop logo' src="https://img.icons8.com/ios-glyphs/60/000000/adobe-photoshop.png"/>
-        <img alt='photoshop logo' src="https://img.icons8.com/ios-glyphs/60/000000/adobe-photoshop.png"/>
-        <img alt='photoshop logo' src="https://img.icons8.com/ios-glyphs/60/000000/adobe-photoshop.png"/>
-        <img alt='photoshop logo' src="https://img.icons8.com/ios-glyphs/60/000000/adobe-photoshop.png"/>
-        <img alt='photoshop logo' src="https://img.icons8.com/ios-glyphs/60/000000/adobe-photoshop.png"/>
+        <img alt='photoshop logo' src="https://github.com/MattDizzle/favicon-host/blob/master/icons/psql3.png?raw=true" />
+        <img alt='photoshop logo' src="https://github.com/MattDizzle/favicon-host/blob/master/icons/ExpressJS.png?raw=true" />
+        <img alt='react logo' src="https://img.icons8.com/office/100/000000/react.png" />
+        <img alt='nodejs logo' src="https://img.icons8.com/color/100/000000/nodejs.png" />
+        <img alt='photoshop logo' src="https://github.com/MattDizzle/favicon-host/blob/master/icons/mongo-db-logo.png?raw=true" />
+        <img alt='heroku logo' src="https://github.com/MattDizzle/favicon-host/blob/master/icons/heroku-logo-1.png?raw=true" />
+        <img alt='photoshop logo' src="https://github.com/MattDizzle/favicon-host/blob/master/icons/Adobe_XD_CC_icon.svg.png?raw=true" />
+        <img alt='photoshop logo' src="https://github.com/MattDizzle/favicon-host/blob/master/icons/Adobe_After_Effects_CC_icon.svg.png?raw=true" />
+        <img alt='photoshop logo' src="https://github.com/MattDizzle/favicon-host/blob/master/icons/audition-logo.png?raw=true" />
+        <img alt='photoshop logo' src="https://github.com/MattDizzle/favicon-host/blob/master/icons/Outlook-Emblem.png?raw=true" />
+
+        {/* <img alt='photoshop logo' src=""/> */}
+
     </>
     return (
         <section>
-            <Card 
-            source='https://source.unsplash.com/collection/10649550' 
-            title='about me' 
-            paragraph="My name is Matthew and I live in Rialto, California. I'm an aspiring full stack web developer.
+            <Card
+                source='https://source.unsplash.com/collection/10649550'
+                title='about me'
+                paragraph="My name is Matthew and I live in Rialto, California. I'm an aspiring full stack web developer.
              I love music, basketball, learning and computers. I recently did an Internship at Eide Bailly where I learned
               a ton of stuff about Integrating Multiple systems using a combination of Boomi and SuiteScript 2.0.
                Prior to this Internship I graduated from the Engineering Immersion Program where I learned Full-Stack Web
                 Development using PostgreSQL, Express, React and Node.js through hands on learning, building multiple apps. "
-            id='about-me' />
+                id='about-me' />
             <Card source='https://source.unsplash.com/collection/1773435' rev='reverse' title='skills' paragraph='' iconz={skillsIcon} id='skills' />
         </section>
 
