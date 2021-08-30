a3c8c2214e3804f27993d6ba5ed4f12415ebb6e6
Matthew Rougely <matthewrougelysr@gmail.com>
2021-08-29 19:44:36 -0700

    changed percents in animation to 1

--- src/components/Code.js
+++ src/components/Code.js
@@ -7,7 +7,7 @@ export default function Code() {
             <Card source='https://raw.githubusercontent.com/MattDizzle/favicon-host/master/saneful/gameview.JPG' title='Saneful' paragraph='Created using Node.js, React, Express and Postgres'/>
             <Card source='https://github.com/M-Sayer/spaced-repetition/blob/master/screenshots/Dash.JPG?raw=true' rev = 'reverse' title='Spaced Repetition' paragraph='Created using Node.js, React, Express and Postgres'/>
             <Card source='https://raw.githubusercontent.com/thinkful-ei-panda/mattr-capstone1-client/master/screenshots/Elections.JPG' title='My Poll Data' paragraph='Created using Node.js, React, Express and Postgres'/>
-            <Card source='https://github.com/MattDizzle/weatherwatch-client/blob/main/Screenshots/Home.JPG?raw=true' rev = 'Weather Watch' paragraph='Created using Node.js and React'/>
+            <Card source='https://github.com/MattDizzle/weatherwatch-client/blob/main/Screenshots/Home.JPG?raw=true' rev = 'reverse' title='Weather Watch' paragraph='Created using Node.js and React'/>
         </div>
     )
 }
