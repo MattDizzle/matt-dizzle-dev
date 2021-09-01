820b6f64eeec7cef4a2affbecb8e27013671e2ad
Matthew Rougely <matthewrougelysr@gmail.com>
2021-09-01 03:50:51 -0700

    changed phone link

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,7 +11,7 @@ export default function Contact() {
         <section className='contact-form-page' id='contact'>
             <div className='image-phone'>
                 <img src='https://github.com/MattDizzle/favicon-host/blob/master/matt-dizzle-dev.JPG?raw=true' className='contact-pic' alt='matt dizzle dev' />
-                <a href="tel:+19095479183" id='phone-link'><FontAwesomeIcon className='icon' icon={faPhone}/> Call Me </a>
+                <a href="tel:+19095479183" id='phone-link'><FontAwesomeIcon className='icon' icon={faPhone}/> Tap here to Call Me </a>
             </div>
             <ContactForm />
         </section>
