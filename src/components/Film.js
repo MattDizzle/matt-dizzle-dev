736daa20254f9d13fb5b1e95846d0993eb97723f
Matthew Rougely <matthewrougelysr@gmail.com>
2021-09-01 03:38:03 -0700

    Added GET request to google API
    Now I am populating the Film page from the YoutTube API...because I can lol

--- src/components/Film.js
+++ src/components/Film.js
@@ -1,12 +1,48 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import VideoCard from './VideoCard'
 
+
+
 export default function Film() {
+    const [data, setData] = useState();
+    useEffect(() => {
+        const options = {
+            // These properties are part of the Fetch Standard
+            method: 'GET',
+            headers: { },            // Request headers. format is the identical to that accepted by the Headers constructor (see below)
+            body: null,             // Request body. can be null, a string, a Buffer, a Blob, or a Node.js Readable stream
+            redirect: 'follow',     // Set to `manual` to extract redirect headers, `error` to reject redirect
+            signal: null,           // Pass an instance of AbortSignal to optionally abort requests
+
+            // The following properties are node-fetch extensions
+            follow: 20,             // maximum redirect count. 0 to not follow redirect
+            compress: true,         // support gzip/deflate content encoding. false to disable
+            size: 0,                // maximum response body size in bytes. 0 to disable
+            agent: null,            // http(s).Agent instance or function that returns an instance (see below)
+            highWaterMark: 16384,   // the maximum number of bytes to store in the internal buffer before ceasing to read from the underlying resource.
+            insecureHTTPParser: false	// Use an insecure HTTP parser that accepts invalid HTTP headers when `true`.
+        }
+
+        const url = `https://www.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_KEY}&channelId=UCOlyZUgsap7nTfX6XsnqUSw&part=id,snippet&order=date&maxResults=20\n`
+
+        fetch(url, options)
+            .then(response => response.json())
+            .then(response => setData(response));
+    }, []);
+
     return (
         <div className='film'>
-            <VideoCard source="https://www.youtube.com/embed/YpXLswNsZqY" title='Reactiv Solutions' paragraph='Voiceover and Editing'/>
-            <VideoCard source="https://www.youtube.com/embed/Cg7BeuxostM" title='Matt Dizzle Dev: Hashtag Generator' paragraph='Coding Interview Practice'/>
-            <VideoCard source="https://www.youtube.com/embed/BmvGcDH88Cs" title='Pig Latin Encoder' paragraph='Coding Interview Practice'/>
+            {
+            data != null ?
+             data.items.map((item, i) =>
+                <VideoCard 
+                style={{ color: 'blue' }} 
+                key={i} 
+                source={`https://www.youtube.com/embed/${item.id.videoId}`} 
+                title={item.snippet.title} description={item.snippet.description} 
+                />) 
+                : 'Loading...'
+            }
         </div>
     )
 }
--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
+
 export default function VideoCard(props) {
 
     return (
         <section className={`video-card animate__animated animate__backInUp ${props.rev}`}>
-            <iframe src={props.source} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe src={props.source} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
             <div className='card-text-box'>
-            <h2>{props.title}</h2>
-            <p>  Sunt eu non et ut est. Nisi quis ea velit dolor aliqua pariatur sit esse magna cillum occaecat. Cupidatat id proident aliqua non. Duis non exercitation veniam minim amet cillum excepteur ipsum nisi do adipisicing. Cupidatat id sint excepteur sunt laboris. Irure exercitation anim Lorem sunt quis tempor consectetur mollit. Enim cillum cillum occaecat culpa excepteur qui.   </p>
+                <h2>{props.title}</h2>
             </div>
         </section>
     )
