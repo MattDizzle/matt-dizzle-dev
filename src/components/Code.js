ba283eba4ad47f3ea9ddd4bc4e2ff5f28727da4a
Matthew Rougely <matthewrougelysr@gmail.com>
2021-09-02 21:03:26 -0700

    Added links to image

--- src/components/Code.js
+++ src/components/Code.js
@@ -3,11 +3,11 @@ import CodeCard from './CodeCard'
 
 export default function Code() {
     return (
-        <div>
+        <div className='code-page'>
             <CodeCard codeLink='https://github.com/MattDizzle/Saneful2020-api' liveLink='https://saneful.vercel.app/' source='https://raw.githubusercontent.com/MattDizzle/favicon-host/master/saneful/gameview.JPG' title='Saneful' paragraph='I used PostgreSQL, React, Express, Node.js, HTML5, CSS and Javascript to create this app.'/>
-            <CodeCard codeLink='https://github.com/M-Sayer/spaced-repetition' liveLink='https://spaced-repetition-brown.vercel.app/register' source='https://github.com/M-Sayer/spaced-repetition/blob/master/screenshots/Dash.JPG?raw=true' rev = 'reverse' title='Spaced Repetition' paragraph='This project was an exercise in using linked lists. I used PostgreSQL, React, Express, Node.js, HTML5, CSS and Javascript to create this app.'/>
+            <CodeCard codeLink='https://github.com/M-Sayer/spaced-repetition' liveLink='https://spaced-repetition-brown.vercel.app/register' source='https://github.com/M-Sayer/spaced-repetition/blob/master/screenshots/Dash.JPG?raw=true' title='Spaced Repetition' paragraph='This project was an exercise in using linked lists. I used PostgreSQL, React, Express, Node.js, HTML5, CSS and Javascript to create this app.'/>
             <CodeCard codeLink='https://github.com/MattDizzle/mattr-capstone1-server' liveLink='https://my-poll-data-client.vercel.app/Home' source='https://raw.githubusercontent.com/thinkful-ei-panda/mattr-capstone1-client/master/screenshots/Elections.JPG' title='My Poll Data' paragraph='I used PostgreSQL, React, Express, Node.js, HTML5, CSS and Javascript to create this app.'/>
-            <CodeCard codeLink='https://github.com/MattDizzle/weatherwatch-client' liveLink='https://weatherwatch.club/' source='https://github.com/MattDizzle/weatherwatch-client/blob/main/Screenshots/Home.JPG?raw=true' rev = 'reverse' title='Weather Watch' paragraph='I designed the logo and the app using the adobe suite. I deployed using Vercel. I used PostgreSQL, React, Express, Node.js, HTML5, CSS and Javascript to create this app.'/>
+            <CodeCard codeLink='https://github.com/MattDizzle/weatherwatch-client' liveLink='https://weatherwatch.club/' source='https://github.com/MattDizzle/weatherwatch-client/blob/main/Screenshots/Home.JPG?raw=true' title='Weather Watch' paragraph='I designed the logo and the app using the adobe suite. I deployed using Vercel. I used PostgreSQL, React, Express, Node.js, HTML5, CSS and Javascript to create this app.'/>
         </div>
     )
 }
--- src/components/CodeCard.js
+++ src/components/CodeCard.js
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCode } from '@fortawesome/free-solid-svg-icons'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+library.add(fab)
 
 export default function CodeCard(props) {
     
     return (
         <section className={`card animate__animated animate__backInUp ${props.rev}`}>
-            <img src= {props.source} className='card-pic' alt='some text' />
+            <a href={props.liveLink}><img src= {props.source} className='card-pic' alt='some text' target="_blank" /></a>
             <div className='card-text-box'>
-            <h2>{props.title}</h2>
+            <h2>{props.title} <a href={props.codeLink} className='code-link' target="_blank"><FontAwesomeIcon icon={faCode}/></a></h2>
             <p>{props.paragraph}</p>
             </div>
         </section>
